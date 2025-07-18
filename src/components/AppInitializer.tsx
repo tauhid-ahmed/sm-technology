7dcb0e102b325bf7c8914e7b51f5d5d16801a56a
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-19 04:28:56 +0600

    feature: app initializer redux helper implement

---
+++ src/components/AppInitializer.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { userApi } from "@/store/services/userApi";
+import type { AppDispatch } from "@/store";
+import { hydrateCartFromStorage } from "@/store/slices/appSlice";
+
+export const AppInitializer = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    const storedCart = localStorage.getItem("cart");
+    if (token) {
+      dispatch(
+        userApi.endpoints.getMe.initiate(undefined, { forceRefetch: true })
+      );
+    }
+    if (storedCart) {
+      dispatch(hydrateCartFromStorage(JSON.parse(storedCart)));
+    }
+  }, [dispatch]);
+
+  return null;
+};
